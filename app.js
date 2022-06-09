b812e81fdcbd0615943110fb71597907355bb90c
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-06-09 19:48:10 -0400

    Added frontend leaderboard. Will add database with Node.js next

--- app.js
+++ app.js
@@ -2,19 +2,49 @@ const gameCanvas = document.querySelector(".canvas");
 const startButton = document.getElementById("start-btn");
 const result = document.getElementById("result");
 const gameState = document.getElementById("game-state");
-
+const leaderboardContainer = document.querySelector(".leaderboard-container");
+const nameInput = document.getElementById("name-input");
+const leaderboardBtn = document.getElementById("leaderboard-btn");
+const restrictText = document.getElementById("restrict-text");
 // Speed and Delay variables
 let delay;
 let speed = 0;
 let speedInterval;
 
 // Start game button handler
-startButton.addEventListener("click", () => {
-  startGame();
+startButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Checks if input is empty
+  if (nameInput.value === "") {
+    restrictText.classList.add("show");
+  } else {
+    restrictText.classList.remove("show");
+    startGame();
+  }
+});
+
+// Handles adding new leaderboard item
+const renderLeaderboard = () => {
+  const container = document.createElement("div");
+  const scoreElement = document.createElement("p");
+  const nameElement = document.createElement("h4");
+  scoreElement.innerText = `Speed: ${speed}ms`;
+  nameElement.innerText = `${nameInput.value}`;
+  leaderboardContainer.appendChild(container);
+  container.appendChild(nameElement);
+  container.appendChild(scoreElement);
+  container.appendChild(leaderboardNumber);
+};
+
+leaderboardBtn.addEventListener("click", () => {
+  leaderboardBtn.classList.remove("show");
+  renderLeaderboard();
 });
 
 // Sets all elements to start game construct
 const startGame = () => {
+  leaderboardBtn.classList.remove("show");
+  restrictText.classList.remove("show");
   speed = 0;
   delay = 0;
   startButton.style.pointerEvents = "none";
@@ -48,5 +78,6 @@ const speedHandler = () => {
   gameCanvas.addEventListener("click", () => {
     resetGame();
     gameState.innerText = "Press start game and click when this turns green";
+    leaderboardBtn.classList.add("show");
   });
 };
